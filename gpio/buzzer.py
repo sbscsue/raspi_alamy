3ec46c612ef5f066deb67dcafea137f209489162
SebinKim <sbscsue@gmail.com>
2021-05-19 04:49:26 +0900

    gpio,alarm

--- gpio/buzzer.py
+++ gpio/buzzer.py
@@ -1,16 +1,18 @@
 import RPi.GPIO as GPIO
 import time
 
-pin_buzzer = 20
+pin_buzzer = 24
 
-
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin_buzzer,GPIO.OUT)
-GPIO.setwarnings(False)
 
-pwm = GPIO.PWM(pin_buzzer,33)
 
-pwm.start(1)
-time.sleep(2)
-GPIO.cleanup()
+while True:
+	GPIO.output(pin_buzzer,GPIO.HIGH)
+	time.sleep(0.5)
+	GPIO.output(pin_buzzer,GPIO.LOW)
+	time.sleep(0.5)
+
+
 
