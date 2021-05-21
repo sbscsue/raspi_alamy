37aaa2e86311221c1f5022eec1447bb778ee74bf
jaeesu <wogmlsu@gmail.com>
2021-05-21 23:51:10 +0900

    image_processing_pixel

---
+++ image_processing/img_hist.cpp
@@ -0,0 +1,12 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv){
+
+	return 0;
+}
