a89efd15d625e4e5fd7b32cf22a8631e70b83d65
jaeesu <wogmlsu@gmail.com>
2021-06-08 21:13:15 +0900

    proc1

---
+++ image_processing/proc1/compare_img/compare_img.py
@@ -0,0 +1,25 @@
+from mkimg.cam import cap_img
+from mkimg.set_img import canny_img
+from cppixel.img_hist import cap_pix
+from labeling.label_img import main_labeling
+
+import argparse
+
+label=''
+new_img='/tmp/sample_img.jpg'
+std_img = "/home/pi/Documents/raspi_alamy/web/weblamy/public/alarm1/img.jpg"
+
+def proc_img():
+    label = main_labeling(std_img)
+
+    cap_img(std_img)
+
+    
+    label_str = main_labeling(new_img)
+    print(label_str)
+
+    if(label_str.find(label) != -1) :
+        cap_pix(canny_img(new_img))
+    
+    print("include : " + str(label_str.find(label)))
+
--- image_processing/proc1/main.py
+++ image_processing/proc1/main.py
@@ -1,26 +1,5 @@
-from mkimg.cam import cap_img
-from mkimg.set_img import canny_img
-from cppixel.img_hist import cap_pix
-from labeling.label_img import main_labeling
-
-import argparse
-
-label='remote control'
-new_img='/tmp/sample_img.jpg'
-std_img = "./etc/co1.jpg"
+from compare_img.compare_img import proc_img
 
 if __name__=='__main__':
-    #capter img
-    cap_img(std_img)
-
-    #img_labeling
     
-    label_str = main_labeling(new_img)
-    print(label_str)
-
-    #img_campare_pixel
-    if(label_str.find(label) != -1) :
-        cap_pix(canny_img(new_img))
-    
-    print("include : " + str(label_str.find(label)))
-
+    proc_img()
